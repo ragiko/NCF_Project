05d5b2f78d66ed9e8183270133839e76f6b751b9
Taguchi Hiroaki <ragiko.complexe@gmail.com>
2014-11-03 05:05:15 +0900

    #6 parseのbaseに移植完了

--- Sample/ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
+++ Sample/ParseStarterProject/src/com/parse/starter/ParseStarterProjectActivity.java
@@ -1,16 +1,46 @@
 package com.parse.starter;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 import com.parse.ParseAnalytics;
 
 public class ParseStarterProjectActivity extends Activity {
+	
+	Button btnStart;
+    Button btnStop;
+    
 	/** Called when the activity is first created. */
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		
+		btnStart = (Button)findViewById(R.id.button1);
+		btnStop = (Button)findViewById(R.id.button2);
+
+		btnStart.setOnClickListener(new ServiceOnClickListener());
+		btnStop.setOnClickListener(new ServiceOnClickListener());
 
 		ParseAnalytics.trackAppOpened(getIntent());
 	}
+	
+	class ServiceOnClickListener implements OnClickListener {
+		@Override
+		public void onClick(View view) {
+			Log.i("MainActivity", "onClick");
+			if( view == btnStart ){
+				Log.i("MainActivity", "btnStart");
+				startService( new Intent( ParseStarterProjectActivity.this, GpsService.class ) );
+			}else if( view == btnStop ){
+				Log.i("MainActivity", "btnStop");
+				stopService( new Intent( ParseStarterProjectActivity.this, GpsService.class ) );
+			}
+		}
+
+    } 
 }
